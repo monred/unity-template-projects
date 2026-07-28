bdec044e983a5df9854a51ccac9292a1cd07bc3a
monred <mon.red@outlook.com>
2026-07-28 10:34:56 -0700

    移动代码

--- Demo/Assets/Scripts/PlayerControl.cs
+++ Demo/Assets/Scripts/PlayerControl.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private bool _grounded;
+    public bool onGrounded;
     public float jumpForce;
     public float horizSpeed;
+    public KeyCode jumpKey;
+    public KeyCode leftKey;
+    public KeyCode squatKey;
+    public KeyCode rightKey;
     // Start is called before the first frame update
     void Awake() {rb = GetComponent<Rigidbody2D>();}
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        int direcion = 0;
+        int jum = 0;
+        if(Input.GetKeyDown(leftKey)) {direcion--;}
+        if(Input.GetKeyDown(rightKey)) {direcion++;}
+        if(Input.GetKeyDown(jumpKey) && onGrounded) {
+            jum = 1;
+        }
+        rb.velocity = new Vector2(direcion * horizSpeed, jum * jumpForce);
         
     }
 }
