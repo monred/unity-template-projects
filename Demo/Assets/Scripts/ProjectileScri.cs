c0caa806a3a9788772fe6361e7b710d475754c57
monred <mon.red@outlook.com>
2026-07-28 11:37:12 -0700

    projectile

---
+++ Demo/Assets/Scripts/ProjectileScri.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScri : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public DamageArea daAr;
+    public Vector3 speed;
+    public Vector3 restPlace;
+    void Awake() {rb = GetComponent<Rigidbody2D>(); daAr = GetComponent<DamageArea>(); }
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        if(daAr.damageTime != daAr.maxDamageTime){
+            rb.velocity = speed;
+        }
+        else{
+            rb.position = restPlace;
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
