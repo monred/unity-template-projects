6caf7de9f4a8b1f566f0deec492d605d15531bb2
WeirdoKo <xama.ko611@gmail.com>
2026-07-28 11:41:35 -0700

    i

---
+++ Demo/Assets/DeathBox.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathBox : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.CompareTag("PlatformerMovement"))
+        {
+            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
