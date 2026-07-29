d670b8019ec2cef3e7f65e72a9d8954730c7b9ec
ZYJLianathris <lukejin9876@gmail.com>
2026-07-29 13:16:40 -0700

    newSkill

---
+++ Demo/Assets/skillObject/FireBall/FireBallTrigger.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool inWork;
+    public Transform player;
+    void Awake() {inWork = 0 == 1;}
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inWork)
+        {
+            transform.position = new Vector3(21.0f, -3.5f, 0.0f);
+            GetComponent<FireShooter>().trigger();
+            inWork = false;
+        }
+    }
+
+    public void GoWork()
+    {
+        if (!inWork)
+        {
+            inWork = true;
+        }
+        return;
+    }
+}
