9a8c86bf2512f7854d65a220fc31597962f9ae8e
ZYJLianathris <lukejin9876@gmail.com>
2026-07-29 11:27:28 -0700

    trap

---
+++ Demo/Assets/skillObject/groundSpike/SpikeTrigger.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool inWork;
+    public Transform player;
+    private int innerRunner;
+    void Awake() {inWork = 0 == 1;}
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inWork)
+        {
+            innerRunner++;
+            if(innerRunner % 300 == 0)
+            {
+                transform.position = new Vector3(player.position.x, -10.0f, player.position.z);
+                GetComponent<FireShooter>().trigger();
+            }
+            if(innerRunner == 1200)
+            {
+                inWork = false;
+            }
+        }
+    }
+
+    public void GoWork()
+    {
+        if (!inWork)
+        {
+            inWork = true;
+            innerRunner = 0;
+        }
+        return;
+    }
+}
