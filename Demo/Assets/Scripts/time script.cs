02fdb7a1f898ce8f50a71602514d131728d00120
Tommy90Or80OnMathQuest <sharkcx6@gmail.com>
2026-07-30 13:39:54 -0700

    d

--- Demo/Assets/Scripts/time script.cs
+++ Demo/Assets/Scripts/time script.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class timescript : MonoBehaviour
+
+
+    
 {
+    public float TimerMaxTimer;
+    public float TimerCurrentTimer;
+    private string sceneToLoad;
+    public TextMeshProUGUI Countdown;
     // Start is called before the first frame update
     void Start()
+      
     {
-        
+        TimerCurrentTimer = TimerMaxTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        TimerCurrentTimer -= Time.deltaTime;
+        Countdown.text = TimerCurrentTimer.ToString();
+        if (TimerCurrentTimer <= 0)
+
+        {
+            SceneManager.LoadScene("Start scene");
+            
+        }
+
+
+
     }
 }
